cb9db9174a7ee35cf478ef1acfc8d0696d089e3d
paulalfille <paulalfille@penguin>
2025-10-23 21:28:38 -0400

    Start of library

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,27 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub trait MessageHeader {
+	
+}
+
+pub struct HeaderSend {
+	version: u32,
+	payload: u32,
+	mtype:   u32,
+	flags:   u32,
+	offset:  u32,
+}
+
+pub struct HeaderReceive {
+	version: u32,
+	payload: u32,
+	ret:     u32,
+	flags:   u32,
+	offset:  u32,
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
